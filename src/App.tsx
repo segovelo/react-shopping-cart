6ba4ca3f9a49bde7c1468889cdd392d6115894bd
segovelo <segovelos@gmail.com>
2021-08-19 03:15:34 +0100

    Changes to App.tsx, Item.styles.ts and Item.tsx files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useQuery } from 'react-query';
 //Components
+import Item from './Item/Item';
 import Drawer from '@material-ui/core/Drawer';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Grid from '@material-ui/core/Grid';
@@ -11,6 +12,7 @@ import { Wrapper } from './App.styles';
 //Types
 export type CartItemType = {
   id: number;
+  title: string;
   category: string;
   description: string;
   image: string;
@@ -22,13 +24,30 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch('https://fakestoreapi.com/products')).json();
 
 const App = () => {
+
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log(data);
   const getTotalItems = () => null;
+  const handleAddToCart = (clickedItem: CartItemType) => null;
   const handleRemoveFromCart = () => null;
   if (isLoading) return <LinearProgress />;
   if (error) return <div> Something went wrong ... </div>;
 
-  return <div className="App">Start</div>
+  return (
+    <Wrapper>
+      <Drawer anchor='right' open={cartOpen} onClose={() => setCartOpen(false)}>
+        Cart goes here
+      </Drawer>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+          </Grid>
+        ))}
+
+      </Grid>
+    </Wrapper>)
 };
 export default App;
