8611369ea6fa4640c6477f5daecb0e170318c596
segovelo <segovelos@gmail.com>
2021-08-19 02:07:02 +0100

    Changes to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Grid from '@material-ui/core/Grid';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
+import { Wrapper } from './App.styles';
 
 const App = () => {
   return <div className="App">Start</div>
