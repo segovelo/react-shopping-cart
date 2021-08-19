01afb255164da9e193a245cad70f06e3d3b24431
segovelo <segovelos@gmail.com>
2021-08-19 02:17:41 +0100

    changes to sart development of new app

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,22 @@ import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 import Badge from '@material-ui/core/Badge';
 // Styles
 import { Wrapper } from './App.styles';
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  amount: number;
+}
+
+const getProducts = async (): Promise<CartItemType[]> =>
+  await (await fetch('https://fakestoreapi.com/products')).json;
 
 const App = () => {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
+  console.log(data);
   return <div className="App">Start</div>
 };
 export default App;
